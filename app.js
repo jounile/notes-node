0475bd74ab8eaa340da6672cf76ae0c72d056f1d
Jouni Leino <jouni.ilari.leino@gmail.com>
2017-08-15 16:41:08 +0200

    Adding CRUD operations

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ console.log('Starting app.');
 const fs = require('fs');
 // const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+
+const argv = yargs.argv;
+var command = process.argv[2];
+// console.log('Command: ', command);
+// console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if(command === 'add'){
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list'){
+  notes.getAll();
+} else if (command === 'read'){
+  notes.getNote(argv.title);
+} else if (command === 'remove'){
+  notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
 
 // console.log(_.isString(true));
 // console.log(_.isString('Jouni'));
