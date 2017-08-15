1de1cca589236a44a09d4ea469f7d5f1e479b47c
Jouni Leino <jouni.ilari.leino@gmail.com>
2017-08-15 15:52:34 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log('Starting app.');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
+
+// console.log(_.isString(true));
+// console.log(_.isString('Jouni'));
+var filteredArray = _.uniq(['Jouni', 1, 'Jouni', 1, 2, 3]);
+console.log(filteredArray);
+
+// var res = notes.addNote();
+// var res = notes.add(9, -2);
+// console.log(res);
+
+// var user = os.userInfo();
+//
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`)
+
+// console.log(module);
