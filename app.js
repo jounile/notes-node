b4caf21aac46c9f9ab9860fc26c67ab0e7281c38
Jouni Leino <jouni.ilari.leino@gmail.com>
2017-08-18 09:19:41 +0200

    Creating Yargs help

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('Starting app.');
+// console.log('Starting app.');
 
 const fs = require('fs');
 // const os = require('os');
@@ -7,12 +7,36 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+}
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+}
 
-const argv = yargs.argv;
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 var command = argv._[0];
+
 // console.log('Command: ', command);
 // console.log('Process', process.argv);
-console.log('Yargs', argv);
+// console.log('Yargs', argv);
 
 if(command === 'add'){
   var note = notes.addNote(argv.title, argv.body);
@@ -25,7 +49,7 @@ if(command === 'add'){
 } else if (command === 'list'){
   var allNotes = notes.getAll();
   console.log(`Printing ${allNotes.length} note(s).`);
-  allNotes.forEach((note) => notes.logNote(note)) 
+  allNotes.forEach((note) => notes.logNote(note))
 } else if (command === 'read'){
   var note = notes.getNote(argv.title);
   if(note){
