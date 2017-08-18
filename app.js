567bd36482a5935add6c3a3da876fdce6e8e1a65
Jouni Leino <jouni.ilari.leino@gmail.com>
2017-08-18 08:55:40 +0200

    List notes changes

--- app.js
+++ app.js
@@ -24,11 +24,8 @@ if(command === 'add'){
   }
 } else if (command === 'list'){
   var allNotes = notes.getAll();
-  if(allNotes){
-    console.log('Notes gotten', allNotes);
-  } else {
-    console.log('Getting notes failed');
-  }
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note)) 
 } else if (command === 'read'){
   var note = notes.getNote(argv.title);
   if(note){
